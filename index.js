4cd312866469375e973b7f5d081e482adaa07d4a
Adedotun Alausa <63458795+adedotunalausa@users.noreply.github.com>
2020-05-21 02:01:20 +0100

    Complete Mobile view with animations
    Added hamburger menu for the menu list and animated it using JavaScript. Animated the content of the website and added smooth scrolling features between sections of the page. Included buttons for the dark and light theme.

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 
-function myFunction() {
+const faders = document.querySelectorAll(".fade-in");
 
-  const myLinks = document.getElementById("my-links");
-  const myName = document.getElementById("my-name");
-  const menuIcon = document.getElementById("menu-icon");
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-items");
+const menuLinks = document.querySelector(".hide-menu");
 
-  if (myLinks.style.display === "block") {
-    myLinks.style.display = "none";
-    myName.innerHTML = "Adedotun";
+// For Hamburger Menu Animation
+menuBtn.addEventListener("click", () => {
+  if(menuBtn.classList.contains("open")) {
+    menuBtn.classList.remove("open");
+    menuItems.classList.remove("show");
+
+    menuItems.classList.add("hide");
+    setTimeout(() => {
+    menuItems.classList.remove("hide");
+  }, 750);
   } else {
-    myLinks.style.display = "block";
-    myName.innerHTML = "Adedotun <br> Alausa";
+    menuBtn.classList.add("open");
+    menuItems.classList.add("show");
+
+    menuItems.classList.remove("hide");
   }
+});
 
-  function iconClick() {
-    menuIcon.classList.add("pressed");
+// For Smooth Appear on Scroll through the page
+const appearOptions = {
+  threshold: 0
+  // rootMargin: "0px 0px -300px 0px"
+};
 
-    setTimeout(function() {
-    menuIcon.classList.remove("pressed");
-  }, 200);
-  }
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
 
-  menuIcon.addEventListener("click", iconClick);
-}
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+// For Smooth Scroll
+$(document).ready(function() {
+  let scrollLink = $(".scroll");
+
+  // Smooth scroll
+  scrollLink.click(function(event) {
+    event.preventDefault();
+    $("body, html").animate({
+      scrollTop: $(this.hash).offset().top
+    },500)
+  });
+});
